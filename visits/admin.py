8a6b86d904999394fc64b0286caf605f039e83e6
Jesus Espino Garcia <jesus.espino@kaleidos.net>
2011-12-13 08:19:07 +0100

    Configuring admin

--- visits/admin.py
+++ visits/admin.py
@@ -3,6 +3,8 @@ from visits.models import Visit
 
 
 class VisitAdmin(admin.ModelAdmin):
-    list_display = ['uri', 'object_app', 'object_model', 'object_id', 'visits']
+    list_display = ['visitor_hash', 'uri', 'object_app', 'object_model', 'object_id', 'visits']
+    list_filter = ['object_app']
+    search_fields = ['object_app', 'object_model', 'object_id', 'uri']
 
 admin.site.register(Visit, VisitAdmin)
